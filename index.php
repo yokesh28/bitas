a3fbfba4dc07fb89065302ed7151c4977244b391
annamalai <annamalai@annamalai-desktop>
2013-07-17 14:55:44 +0530

    Signed-off-by: annamalai <annamalai@annamalai-desktop>

---
+++ index.php
@@ -0,0 +1,268 @@
+<!DOCTYPE html>
+<html>
+  <head>
+     
+    <meta charset="utf-8">
+    <title>Vitas</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    <link rel="stylesheet" href="css/font-awesome.css">
+    <link href="css/bootstrap.css" rel="stylesheet">        
+    <link href="css/bootstrap-responsive.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/flexslider.css">
+    
+    <link href="css/style.css" rel="stylesheet" media="screen">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="../assets/js/html5shiv.js"></script>
+    <![endif]-->
+
+    <!-- Fav and touch icons -->
+   
+      <!--   <link rel="shortcut icon" href="../assets/ico/favicon.png">-->
+       
+       <style type="text/css">
+    .input-append button.add-on { 
+        height: inherit !important;
+    }
+</style>
+      <script src="http://code.jquery.com/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script> 
+       
+       
+  </head>
+  <body>
+    
+    
+    <div class="container">
+    <div class="container">
+            <div class="header row">
+                <div class="span12">
+                   <form class="navbar-form pull-right">
+                       <img src="img/search.png">
+                       <p><img src="img/icons/icon-18-phone.png" alt="icon">&nbsp;(209)910-3468 &nbsp;&nbsp;<img src="img/icons/icon-18-mail.png" alt="icon">&nbsp;<a href="">info@vitas.com</a></p>
+                   </form>
+              
+        
+        
+        
+        
+  <!-- nav header -->      
+        
+        
+      
+           
+                    <div class="navbar">
+                        <div class="navbar-inner">                                                    
+                              <a  href="index.php"><img class="brand"  alt="" src="img/logo.png"></a>
+                              <div class="nav-collapse collapse">
+                                <ul id="nav" class="nav">
+                                    <li class="active">
+                                        <a href="index.php">Home</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Project</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">About us</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Gallery</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Media Center</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Contact Us</a>
+                                    </li>
+                                </ul>
+                            </div>
+                                                       
+                        </div>
+                    </div>
+                    
+                    
+            
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                </div>
+            </div>
+        </div>
+        
+                
+              <!-- Slider -->
+        <div class="slider">
+            <div class="container">
+            
+                <div class="row">
+                    <div class="span12">
+                        <div class="flexslider">
+                            <ul class="slides">
+                                <li >
+                                    <img src="img/slider/1.jpg">
+                                    
+                                </li>
+                                <li>
+                                    <img src="img/slider/2.jpg">
+                                    
+                                </li>
+                               
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>       
+                    
+           <!-- inner part -->
+           
+           <div class="container" style="margin-bottom: 30px;">
+              <div class="row">
+                 <div class="span6 welcome-vitas">
+             
+          <h2>Heading</h2><hr>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, 
+          tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta 
+          sem malesuada magna mollis euismod. Donec sed odio dui.
+          Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, 
+          tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta 
+          sem malesuada magna mollis euismod. Donec sed odio dui.
+          Donec sed odio dui.
+          Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, 
+          tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta 
+          sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                  </div>
+               
+              
+              <div class="span3">
+             <img alt="" src="img/on1.png">
+              
+              </div>    
+              
+               
+              <div class="span3">
+             <img alt="" src="img/up1.png">
+              
+              </div> 
+              </div>       
+           
+           </div>
+           
+           
+           
+    </div>       
+           
+           
+           
+           
+           
+           
+           
+                    
+       <!-- footer --> 
+       
+        <footer id="footer" class="clearfix">
+  <div class="container"> 
+
+  
+  <!--Twitter-->
+   
+
+    <!--End Twitter-->
+
+
+    <!--spacer here-->
+    <div class="spacer-30px"></div>
+    
+    <!--row starts-->
+    <div class="row"> 
+      
+      <!--column one starts-->
+      <div class="span5 about-text">
+        <h5>About </h5>
+        <p>From simple point-based incentive programs to complex multi-tiered loyalty environments, DotsCard
+         provides the expertise, technology and program support services to bring your loyalty program to 
+         life and achieve any of your goals.</p>
+         
+       </div>
+      <!--column one ends--> 
+      
+      <!--column two starts-->
+      <div class="span4">
+     
+          <h5>Twitter Feeds</h5>
+        <div id="twitter"><ul class="tweetList"></ul></div>
+      </div>
+      <!--column two ends--> 
+      
+      <!--column three starts-->
+      <div class="span3">
+        <h5 style="margin-left: 23px;">Get in Touch</h5>
+        <ul class="list-info">
+          <li><img src="img/icons/icon-18-address.png" alt="icon">&nbsp;
+            P.O.Box 25214<br>
+            Dubai - UAE</li>
+          <li><img src="img/icons/icon-18-phone.png" alt="icon">&nbsp;(209)910-3468</li>
+          <li><img src="img/icons/icon-18-mail.png" alt="icon">&nbsp;<a href="info@vitas.com">info@vitas.com</a></li>
+         
+        </ul>
+      </div>
+      <div class="social span6">
+    
+             <h5 class="social-text">Follow us</h5>       <a class="facebook" href="#"></a>
+                        <a class="dribbble" href="#"></a>
+                        <a class="twitter" href="#"></a>
+                      
+                       
+                    </div>
+      <!--column three ends--> 
+      
+    </div>
+    <!--row ends--> 
+    
+  </div>
+</footer>
+
+
+<section id="copyright" class="clearfix">
+  <div class="container">
+    <div class="row">
+      <div class="span12">
+        <ul class="copyright-header">
+           <li> <a href="index.php">Home</a> </li>
+            <li>  <a href="#">Project</a>   </li>                       
+             <li>   <a href="#">About us</a>   </li>                  
+             <li>     <a href="#">Gallery</a>   </li>                
+              <li>    <a href="#">Media Center</a>   </li>                     
+                <li>    <a href="#">Contact Us</a>   </li>                           
+                        </ul>
+        
+        <!--copyright menu starts-->
+        <ul class="copyright-menu">
+          <li><a href="#">Terms &amp; Conditions</a></li>
+          <li><a href="#">Privacy Policy</a></li>
+        
+        </ul>
+        <!--copyright menu ends--> 
+        
+      </div>
+    </div>
+  </div>
+</section>
+
+      
+        <script src="js/jquery.flexslider.js"></script>   
+         <script src="js/scripts.js"></script>     
+       
+    
+  </body>
+</html>
