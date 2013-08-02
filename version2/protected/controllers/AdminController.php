81b18bb45a2fbe7aad89f5bf3612dd78429183f6
lokesh <lokesh@lokesh-HP-Compaq-dc7800-Convertible-Minitower>
2013-08-02 16:15:05 +0530

    Signed-off-by: lokesh <lokesh@lokesh-HP-Compaq-dc7800-Convertible-Minitower>

--- version2/protected/controllers/AdminController.php
+++ version2/protected/controllers/AdminController.php
@@ -10,34 +10,69 @@ class AdminController extends Controller
 	 */
 	public function actionLogin()
 	{
+		if(!Yii::app()->user->isGuest) 
+			$this->redirect($this->createAbsoluteUrl('admin/index'));
+		
 		$model=new LoginForm;
-	
+
 		// if it is ajax validation request
 		if(isset($_POST['ajax']) && $_POST['ajax']==='login-form')
 		{
 			echo CActiveForm::validate($model);
 			Yii::app()->end();
 		}
-	
+
 		// collect user input data
 		if(isset($_POST['LoginForm']))
 		{
 			$model->attributes=$_POST['LoginForm'];
 			// validate user input and redirect to the previous page if valid
 			if($model->validate() && $model->login())
-				$this->redirect(Yii::app()->user->returnUrl);
+				$this->redirect($this->createAbsoluteUrl('admin/index'));
 		}
 		// display the login form
 		$this->render('login',array('model'=>$model));
 	}
-	
+
 	/**
 	 * Logs out the current user and redirect to homepage.
 	 */
 	public function actionLogout()
 	{
 		Yii::app()->user->logout();
-		$this->redirect(Yii::app()->homeUrl);
+		$this->redirect($this->createAbsoluteUrl('admin/login'));
 	}
+
+	/**
+	 * @return array action filters
+	 */
+	public function filters()
+	{
+		return array(
+				'accessControl', // perform access control for CRUD operations
+		);
+	}
+
+	/**
+	 * Specifies the access control rules.
+	 * This method is used by the 'accessControl' filter.
+	 * @return array access control rules
+	 */
+	public function accessRules()
+	{
+		return array(
+				array('allow',  // allow all users to access 'index' and 'view' actions.
+						'actions'=>array('login'),
+						'users'=>array('*'),
+				),
+				array('allow', // allow authenticated users to access all actions
+						'users'=>array('@'),
+				),
+				array('deny',  // deny all users
+						'users'=>array('*'),
+				),
+		);
+	}
+
 }
 ?>
